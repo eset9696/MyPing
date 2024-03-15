1997dd4ea1c7fb53c2cb1128e67e758c6925e3d4
sherkunkov@gmail.com <sherkunkov@gmail.com>
2024-03-15 22:12:35 +0800

    in progress

--- MyPing/Form1.cs
+++ MyPing/Form1.cs
@@ -18,16 +18,16 @@ namespace MyPing
 	public partial class Form1 : Form
 	{
 		Thread updateThread;
-		Ping pingSender = new Ping();
-		PingOptions options = new PingOptions();
+		
 		int timeout = 120;
 
 		byte[] data = new byte[32];
-		List<string> ipAddresses = new List<string>();
+		List<Node> Nodes = new List<Node>();
 		public Form1()
 		{
 			InitializeComponent();
 			InitListView();
+			
 		}
 
 		void InitListView()
@@ -36,36 +36,76 @@ namespace MyPing
 			lvIpAddresses.Columns.Add("Bytes");
 			lvIpAddresses.Columns.Add("Time");
 			lvIpAddresses.Columns.Add("TTL");
+			lvIpAddresses.Columns.Add("Sent");
+			lvIpAddresses.Columns.Add("Received");
+			lvIpAddresses.Columns.Add("Lost");
 		}
 
-		void StartSend()
+		void Send()
+		{
+            for (int i = 0; i < Nodes.Count; i++)
+            {
+                Ping pingSender = new Ping();
+                PingOptions options = new PingOptions();
+                PingReply reply = pingSender.Send(Nodes[i].IpAddress, timeout, data, options);
+                if (reply.Status == IPStatus.Success)
+                {
+                    lvIpAddresses.Items[i].SubItems.Add(reply.Buffer.Length.ToString());
+                    lvIpAddresses.Items[i].SubItems.Add(reply.RoundtripTime.ToString());
+                    lvIpAddresses.Items[i].SubItems.Add(reply.Options.Ttl.ToString());
+					lvIpAddresses.Items[i].SubItems.Add(Nodes[i].Sent.ToString());
+					lvIpAddresses.Items[i].SubItems.Add(Nodes[i].Received.ToString());
+					lvIpAddresses.Items[i].SubItems.Add(Nodes[i].Lost.ToString());
+                }
+                if (reply.Status == IPStatus.TimedOut)
+                {
+					lvIpAddresses.Items[i].SubItems[.Add((Nodes[i].Lost + 1).ToString());
+                }
+            }
+        }
+
+		void RefreshData()
 		{
 			while (lvIpAddresses.Items.Count > 0)
 			{
-				for (int i = 0; i < ipAddresses.Count; i++)
+				for (int i = 0; i < lvIpAddresses.Items.Count; i++)
 				{
-					PingReply reply = pingSender.Send(ipAddresses[i], timeout, data, options);
+                    Ping pingSender = new Ping();
+                    PingOptions options = new PingOptions();
+                    PingReply reply = pingSender.Send(Nodes[i].IpAddress.ToString(), timeout, data, options);
 					if (reply.Status == IPStatus.Success)
 					{
-						lvIpAddresses.Items[i].SubItems.Add(reply.Buffer.Length.ToString());
-						lvIpAddresses.Items[i].SubItems.Add(reply.RoundtripTime.ToString());
-						lvIpAddresses.Items[i].SubItems.Add(reply.Options.Ttl.ToString());
+                        if (InvokeRequired)
+						{
+                            this.Invoke(new Action(() => lvIpAddresses.Items[i].SubItems.Add(reply.Buffer.Length.ToString())));
+                            this.Invoke(new Action(() => lvIpAddresses.Items[i].SubItems[2].Text = reply.RoundtripTime.ToString()));
+                            this.Invoke(new Action(() => lvIpAddresses.Items[i].SubItems[3].Text = reply.Options.Ttl.ToString()));
+                            this.Invoke(new Action(() => lvIpAddresses.Items[i].SubItems[4].Text = (Convert.ToInt32(lvIpAddresses.Items[i].SubItems[4].Text) + 1).ToString()));
+                            this.Invoke(new Action(() => lvIpAddresses.Items[i].SubItems[5].Text = (Convert.ToInt32(lvIpAddresses.Items[i].SubItems[5].Text) + 1).ToString()));
+                            
+                        }
 					}
-				} 
+					if (reply.Status == IPStatus.TimedOut)
+					{
+						this.Invoke(new Action(() => lvIpAddresses.Items[i].SubItems[6].Text = (Convert.ToInt32(lvIpAddresses.Items[i].SubItems[6].Text) + 1).ToString()));
+					}
+				}
+				Thread.Sleep(1000);
 			}
 		}
 
-		void Update()
+		void UpdateData()
 		{
-			updateThread = new Thread(StartSend);
+			updateThread = new Thread(RefreshData);
 			updateThread.Start();
 		}
 
 		private void btnAddIp_Click(object sender, EventArgs e)
 		{
-			ipAddresses.Add(rtbIpAddress.Text);
+			Nodes.Add(new Node(rtbIpAddress.Text));
 			lvIpAddresses.Items.Add(rtbIpAddress.Text);
-			Update();
+			Send();
+			UpdateData();
 		}
 
 		private void btnDeleteIp_Click(object sender, EventArgs e)
---
+++ MyPing/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPing
+{
+    internal class Node
+    {
+        public string IpAddress { get; set; }
+        public uint Sent { get; set; }
+        public uint Received { get; set; }
+        public uint Lost { get; set; }
+        public Node(string ipAddress) 
+        { 
+            IpAddress = ipAddress; 
+            Sent = 0;
+            Received = 0;
+            Lost = 0;
+        }
+    }
+}
